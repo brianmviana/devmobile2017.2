3fa27715031bdc289c74fd8609ffa641cb9c2c83
Anibal Cavalcante de Olivera <hanibal.ce80@gmail.com>
2017-09-27 19:03:12 -0300

    Ajuste nos imports do DatePickerFragment.

--- DatePickerExemplo/app/src/main/java/br/ufc/quixada/datepickerexemplo/DatePickerFragment.java
+++ DatePickerExemplo/app/src/main/java/br/ufc/quixada/datepickerexemplo/DatePickerFragment.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.Context;
 import android.os.Bundle;
 import android.widget.DatePicker;
 
