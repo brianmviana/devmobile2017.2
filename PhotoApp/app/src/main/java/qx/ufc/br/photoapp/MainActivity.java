7886311cf244b888db2ae9a74f5d804b2b0fd318
Anibal Oliveira <hanibal.ce80@gmail.com>
2017-11-17 14:51:39 -0300

    Corrigindo para versão 6.0 do Android.

--- PhotoApp/app/src/main/java/qx/ufc/br/photoapp/MainActivity.java
+++ PhotoApp/app/src/main/java/qx/ufc/br/photoapp/MainActivity.java
@@ -1,13 +1,16 @@
 package qx.ufc.br.photoapp;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -23,6 +26,42 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void getPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        } else
+            dispatchTakePictureIntent();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    dispatchTakePictureIntent();
+                } else {
+                    Toast.makeText(this, "Não vai funcionar!!!", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+    public void capturarImagem(View v) {
+        getPermissions();
+    }
+
+    private void dispatchTakePictureIntent() {
+        File diretorio = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String nomeImagem = diretorio.getPath() + "/" + System.currentTimeMillis() + ".jpg";
+        uri = Uri.fromFile(new File(nomeImagem));
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        startActivityForResult(intent, CAPTURAR_IMAGEM);
     }
 
     @Override
@@ -42,17 +81,5 @@ public class MainActivity extends Activity {
                         .show();
             }
         }
-    }
-
-
-    public void capturarImagem(View v) {
-        File diretorio = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-
-        String nomeImagem = diretorio.getPath() + "/" + System.currentTimeMillis() + ".jpg";
-        uri = Uri.fromFile(new File(nomeImagem));
-
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
-        startActivityForResult(intent, CAPTURAR_IMAGEM);
     }
 }
